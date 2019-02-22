a5b07477800763bea6c65ed10a8332cb738afcd1
Luis Cruz <luismirandacruz@gmail.com>
2019-02-22 21:49:04 +0000

    auto generating readme.md

--- build.sh
+++ build.sh
@@ -1,4 +1,3 @@
 bundle exec jekyll build
-bundle exec reverse_markdown --unknown_tags=drop ./_site/index.html > readme.md
-ruby -e "puts readlines.join.gsub(/\- \n\n/, '- ')" readme.md > readme.md
+bundle exec reverse_markdown --unknown_tags=drop ./_site/index.html | ruby -e "puts readlines.join.gsub(/\- \n\n/, '- ')" | cat > readme.md
 echo "Awesome list was built successfully."
